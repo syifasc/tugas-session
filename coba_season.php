967c3b918a11b298f34bae584b01e5d03d7f7b03
syifasc <112141501+syifasc@users.noreply.github.com>
2022-08-31 11:07:39 +0700

    Add files via upload

---
+++ coba_season.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+$_SESSION["nama"] = "cipacantik";
+$_SESSION["id"]  = "1234567";
---
+++ form_session.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get"action="coba_tampil_session.php">
+        <P>username<input name="udername" type="text"></p>
+        <P>id <input name="id" type="number"></P>
+
+        <button type="submit">Prosses</nutton>
+</body>
+</html>
